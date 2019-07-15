6f69a0b0779ed825b5158663d01b4c967171be97
Katie Sadoff <katie.sadoff@mongodb.com>
2019-07-15 13:54:29 -0400

    Created stubs for MongoVertex

--- Mongopop/src/main/java/gremlin/structure/MongoVertex.java
+++ Mongopop/src/main/java/gremlin/structure/MongoVertex.java
@@ -1,4 +1,46 @@
 package gremlin.structure;
 
-public class MongoVertex {
+import org.apache.tinkerpop.gremlin.structure.*;
+
+import java.util.Iterator;
+
+public class MongoVertex implements Vertex {
+
+    public Edge addEdge(String label, Vertex inVertex, Object... keyValues) {
+        return null;
+    }
+
+    public <V> VertexProperty<V> property(VertexProperty.Cardinality cardinality, String key, V value, Object... keyValues) {
+        return null;
+    }
+
+    public Iterator<Edge> edges(Direction direction, String... edgeLabels) {
+        return null;
+    }
+
+    public Iterator<Vertex> vertices(Direction direction, String... edgeLabels) {
+        return null;
+    }
+
+    public Object id() {
+        return null;
+    }
+
+    public String label() {
+        return null;
+    }
+
+    public Graph graph() {
+        return null;
+    }
+
+    public void remove() {
+
+    }
+
+    public <V> Iterator<VertexProperty<V>> properties(String... propertyKeys) {
+        return null;
+    }
+
+
 }
