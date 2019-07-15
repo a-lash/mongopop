c391b2d097b604c2210afdf3d73a922cf3676d90
Katie Sadoff <katie.sadoff@mongodb.com>
2019-07-15 13:57:50 -0400

    Added todo comments

--- Mongopop/src/main/java/gremlin/structure/MongoVertex.java
+++ Mongopop/src/main/java/gremlin/structure/MongoVertex.java
@@ -7,38 +7,46 @@ import java.util.Iterator;
 public class MongoVertex implements Vertex {
 
     public Edge addEdge(String label, Vertex inVertex, Object... keyValues) {
+        //TODO(implement)
         return null;
     }
 
     public <V> VertexProperty<V> property(VertexProperty.Cardinality cardinality, String key, V value, Object... keyValues) {
+        //TODO(implement)
         return null;
     }
 
     public Iterator<Edge> edges(Direction direction, String... edgeLabels) {
+        //TODO(implement)
         return null;
     }
 
     public Iterator<Vertex> vertices(Direction direction, String... edgeLabels) {
+        //TODO(implement)
         return null;
     }
 
     public Object id() {
+        //TODO(implement)
         return null;
     }
 
     public String label() {
+        //TODO(implement)
         return null;
     }
 
     public Graph graph() {
+        //TODO(implement)
         return null;
     }
 
     public void remove() {
-
+        //TODO(implement)
     }
 
     public <V> Iterator<VertexProperty<V>> properties(String... propertyKeys) {
+        //TODO(implement)
         return null;
     }
 
