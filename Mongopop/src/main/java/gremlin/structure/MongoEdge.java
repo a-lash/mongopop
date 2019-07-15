263c6c18a70d0d66195a8540a32b354ee2f1d6ec
Rishab Chander <rishab.chander@mongodb.com>
2019-07-15 13:48:36 -0400

    added stubs for MongoEdge

--- Mongopop/src/main/java/gremlin/structure/MongoEdge.java
+++ Mongopop/src/main/java/gremlin/structure/MongoEdge.java
@@ -1,4 +1,43 @@
 package gremlin.structure;
 
-public class MongoEdge {
+import java.util.Iterator;
+
+import org.apache.tinkerpop.gremlin.structure.*;
+
+public class MongoEdge implements Edge {
+    public Object id() {
+        //TODO(implement)
+        return null;
+    }
+
+    public Iterator<Vertex> vertices(Direction direction) {
+        //TODO(implement)
+        return null;
+    }
+
+    public Graph graph() {
+        //TODO(implement)
+        return null;
+    }
+    
+    public String label() {
+        //TODO(implement)
+        return null;
+    }
+
+    public void remove() {
+        //TODO(implement)
+    }
+
+    public <V> Iterator<Property<V>> properties(String... propertyKeys) {
+        //TODO(implement)
+        return null;
+    }
+
+    public <V> Property<V> property(String key, V value) {
+        //TODO(implement)
+        return null;
+    }
+
+
 }
