7f327e785aa4e161da9915b2f38bbfe171e40133
Anya Hargil <anya.hargil@Anyas-MacBook-Pro.local>
2019-07-15 18:02:19 -0400

    Translates MongopGraph from Kotlin to Java (still has errors)

--- Mongopop/src/main/java/gremlin/structure/MongoGraph.java
+++ Mongopop/src/main/java/gremlin/structure/MongoGraph.java
@@ -1,6 +1,15 @@
 package gremlin.structure;
 
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.stream.Collectors;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 
 import org.apache.commons.configuration.Configuration;
 import org.apache.tinkerpop.gremlin.process.computer.GraphComputer;
@@ -8,23 +17,53 @@ import org.apache.tinkerpop.gremlin.structure.Edge;
 import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.apache.tinkerpop.gremlin.structure.Transaction;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraphVariables;
+import org.bson.Document;
 
 public class MongoGraph implements Graph {
 
+    private final MongoClient client;
+    private final MongoCollection<Document> vertices;
+    private final MongoCollection<Document> edges;
+    private final MongoDatabase db;
+    private final TinkerGraphVariables variables;
+    private final Configuration conf;
+
+    private static final String MONGODB_CONFIG_PREFIX = "gremlin.mongodb";
+
+    protected MongoGraph(Configuration conf) {
+        ConnectionString url = new ConnectionString(conf.getString(MONGODB_CONFIG_PREFIX + ".connectionUrl"));
+        this.client = MongoClients.create(url);
+        this.db = client.getDatabase(url.getDatabase());
+        this.vertices = db.getCollection("vertices");
+        this.edges = db.getCollection("edges");
+        this.variables = new TinkerGraphVariables();
+        this.conf = conf;
+    }
+
     public Vertex addVertex(Object... keyValues) {
-        return null;
+        MongoVertex mongoVertex = new MongoVertex(this, *keyValues);
+        mongoVertex.save();
+
+        return mongoVertex;
     }
 
     public <C extends GraphComputer> C compute(Class<C> graphComputerClass) throws IllegalArgumentException {
+        //TODO(implemented)        
         return null;
     }
 
     public GraphComputer compute() throws IllegalArgumentException {
+        //TODO(implemented)        
         return null;
     }
 
     public Iterator<Vertex> vertices(Object... vertexIds) {
-        return null;
+        String[] ids = Arrays.copyOf(vertexIds, vertexIds.length, String[].class);
+        if (vertexIds.length == 0) {
+            return ((Iterator<Vertex>) (vertices.find().map(it -> new MongoVertex(it, this))));
+        }
+        return ((Iterator<Vertex>) vertices.find(Filters.in("_id", ids)).map(it -> new MongoVertex(it, this)));
     }
 
     public Iterator<Edge> edges(Object... edgeIds) {
@@ -32,19 +71,20 @@ public class MongoGraph implements Graph {
     }
 
     public Transaction tx() {
+        //TODO(implemented)
         return null;
     }
 
     public void close() throws Exception {
-
+        client.close();
     }
 
     public Variables variables() {
-        return null;
+        return variables;
     }
 
     public Configuration configuration() {
-        return null;
+        return conf;
     }
 
 }
