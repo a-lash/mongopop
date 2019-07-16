9ed08e6e76b7cefd4275fbd880151e5b7e2fdd58
Anya Hargil <anya.hargil@Anyas-MacBook-Pro.local>
2019-07-16 13:36:02 -0400

    Stubs out MongoProperty and translates MongoEdge

--- Mongopop/src/main/java/gremlin/structure/MongoProperty.java
+++ Mongopop/src/main/java/gremlin/structure/MongoProperty.java
@@ -1,4 +1,46 @@
+/*
+ * Copyright 2008-present MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package gremlin.structure;
 
-public class MongoProperty {
+import org.apache.tinkerpop.gremlin.structure.Element;
+import org.apache.tinkerpop.gremlin.structure.Property;
+
+public class MongoProperty<T> implements Property<T> {
+    public Element element() {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return null;
+    }
+
+    public void remove() {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    public T value() {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return null;
+    }
+
+    public boolean isPresent() {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return false;
+    }
+
+    public String key() {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return null;
+    }
 }
